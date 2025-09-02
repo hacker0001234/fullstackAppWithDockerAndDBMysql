a7cf5078cca3a7d05e3e9e9e4494a4407f20927a
Mykola <mykola.chichkalenko@gmail.com>
2025-09-02 15:58:53 +0300

    changed Tables

--- backend/src/main/java/com/example/fullStack/Configs/Tables.java
+++ backend/src/main/java/com/example/fullStack/Configs/Tables.java
@@ -1,23 +1,29 @@
 package com.example.fullStack.Configs;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Tables {
+    private final DatabaseClient databaseClient;
+    public Tables( DatabaseClient databaseClient1){
 
-    public Tables(DatabaseClient databaseClient){
 
-        databaseClient.sql(
-                "CREATE TABLE IF NOT EXISTS users (" +
-                        "id INT AUTO_INCREMENT PRIMARY KEY, " +
-                        "name VARCHAR(100) NOT NULL, " +
-                        "email VARCHAR(100) NOT NULL UNIQUE, " +
-                        "password VARCHAR(100) NOT NULL" +
-                        ");"
+        this.databaseClient = databaseClient1;
+    }
+
+    @PostConstruct
+    public Void init() {
+       return databaseClient.sql(
+                        "CREATE TABLE IF NOT EXISTS users (" +
+                                "id INT AUTO_INCREMENT PRIMARY KEY, " +
+                                "name VARCHAR(100) NOT NULL, " +
+                                "email VARCHAR(100) NOT NULL UNIQUE, " +
+                                "password VARCHAR(100) NOT NULL" +
+                                ");"
                 )
                 .then()
                 .block();
-
     }
 }
